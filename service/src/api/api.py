b96f9b111f5fefcf39817807a935f33bbbead429
KaitoKurokochi <kaito.24.kurokochi.09@gmail.com>
2025-04-08 17:27:52 +0900

    :recycle: requirementsなどのファイルを追加

--- service/src/api/api.py
+++ service/src/api/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import users, items
+# from app.api.endpoints import users, items
 
 api_router = APIRouter()
 api_router.include_router(users.router, tags=["users"], prefix="/users") # tagsやprefixは設計に応じて付与
--- service/src/config.py
+++ service/src/config.py
@@ -6,3 +6,5 @@ import os
 DB_NAME = os.getenv('DB_NAME') 
 COLLECTION_NAME = os.getenv('COLLECTION_NAME') 
 DB_URI = os.getenv('mongodb://localhost:27017/')
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+SERVICE_PORT = int(os.getenv("SERVICE_PORT"))
