fb87ce2d13e646f4ae6113bd77edb9ed11154b5b
Haruki Kurokawa <ha.brack96@gmail.com>
2025-04-18 15:06:09 +0900

    :+1: NavigationBarを追加した

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { NavBar } from '@/components/NavBar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -24,7 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="w-full h-full">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased w-full h-full`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased w-full h-full flex flex-col`}
+      >
+        <NavBar />
         {children}
       </body>
     </html>
---
+++ web/src/components/SideDrawer/SideDrawer.tsx
@@ -0,0 +1,37 @@
+export const SideDrawer = () => {
+  return (
+    <div className="drawer">
+      <input id="side-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        <label htmlFor="side-drawer" className="btn btn-square btn-ghost drawer-button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-5 w-5 stroke-current"
+          >
+            {' '}
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>{' '}
+          </svg>
+        </label>
+      </div>
+      <div className="drawer-side z-50">
+        <label htmlFor="side-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+          {/* Sidebar content here */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
