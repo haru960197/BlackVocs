ba2cda310a59e1a0552b0635ab3d797c08c659fe
haru-lab-pc <ha.brack96@gmail.com>
2025-08-13 20:57:15 +0900

    :bug: serveractionsでCookieが取れない問題を解消

--- web/src/hey-api.ts
+++ web/src/hey-api.ts
@@ -2,5 +2,5 @@ import type { CreateClientConfig } from './lib/api/client.gen';
 
 export const createClientConfig: CreateClientConfig = (config) => ({
   ...config,
-  baseUrl: 'http://127.0.0.1:4000',
+  baseUrl: 'http://localhost:4000',
 });
