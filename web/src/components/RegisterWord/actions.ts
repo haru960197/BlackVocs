9c2a11a5c29e613f7bd640beb1c49dd67c2c9833
haru-lab-pc <ha.brack96@gmail.com>
2025-08-13 22:44:31 +0900

    :bug: 単語登録ができるようになった

--- web/src/components/RegisterWord/actions.ts
+++ web/src/components/RegisterWord/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { addNewWord, AddNewWordError, AddNewWordResponse } from "@/lib/api";
+import { cookies } from "next/headers";
 
 /**
  * 英単語を登録する
@@ -11,9 +12,19 @@ export const registerNewWord = async (word: string): Promise<{
   error?: AddNewWordError;
   data?: AddNewWordResponse;
 }> => {
+  const cookieStore = await cookies();
+  const tokenCookie = cookieStore.get('access_token');
+
+  if (!tokenCookie) {
+    return { success: false };
+  }
+
   const res = await addNewWord({
     body: {
       word,
+    },
+    headers: {
+      Cookie: `${tokenCookie.name}=${tokenCookie.value}`,
     },
   });
 
