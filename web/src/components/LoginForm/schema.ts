9335a55dec3a887b9193b8c41961fc49e7e5760b
haru-lab-pc <ha.brack96@gmail.com>
2025-08-15 13:53:07 +0900

    :recyle: パスワードの文字列に#を許容するように変更

--- web/src/components/LoginForm/schema.ts
+++ web/src/components/LoginForm/schema.ts
@@ -5,7 +5,7 @@ export const loginSchema = z.object({
   password: z
     .string()
     .min(8, { message: 'パスワードは8文字以上で入力してください' })
-    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{8,}$/, {
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$#!%*?&]{8,}$/, {
       message: 'パスワードは、大文字、小文字、数字をそれぞれ1つ以上含んでください。',
     }),
 });
