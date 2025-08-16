33507a0260920345adc30f08f3914d30bc4520a8
Kaito Kurokochi <kaito.24.kurokochi.09@gmail.com>
2025-08-16 21:33:40 +0900

    :recycle: デプロイのために修正

--- service/app/repositories/session.py
+++ service/app/repositories/session.py
@@ -1,16 +1,9 @@
 from urllib import parse
-from core.config import db_config
+from core.config import MONGO_DB_URL
 from pymongo import MongoClient
 from pymongo.database import Database
 
-USER_NAME = parse.quote_plus(db_config.DB_USERNAME)
-PASSWORD = parse.quote_plus(db_config.DB_PASSWORD)
-HOST = db_config.DB_HOST
-PORT = db_config.DB_PORT
-
-MONGO_DATABASE_URL: str = f"mongodb://{USER_NAME}:{PASSWORD}@{HOST}:{PORT}"
-
-client = MongoClient(MONGO_DATABASE_URL)
+client = MongoClient(MONGO_DB_URL)
 
 def get_db() -> Database:
     db: Database = client.db
