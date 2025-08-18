617d7bf4b77f50b808a98d622c1e150688e99a2b
kuro-home-pc <ha.brack96@gmail.com>
2025-08-18 21:22:12 +0900

    :cop: 単語登録スキーマを修正

--- web/src/components/RegisterWord/schema.ts
+++ web/src/components/RegisterWord/schema.ts
@@ -1,10 +1,22 @@
 import { z } from 'zod';
 
 export const wordFormSchema = z.object({
-  word: z.string().min(1, { message: '単語は必須です' }),
-  meaning: z.string().min(1, { message: '意味は必須です' }),
-  example: z.string().min(1, { message: '例文は必須です' }),
-  exampleTranslation: z.string().min(1, { message: '例文の日本語訳は必須です' }),
+  word: z
+    .string()
+    .max(45, { message: '45文字以内で入力してください' })
+    .min(1, { message: '単語は必須です' }),
+  meaning: z
+    .string()
+    .max(45, { message: '45文字以内で入力してください' })
+    .min(1, { message: '意味は必須です' }),
+  example: z
+    .string()
+    .max(100, { message: '100文字以内で入力してください' })
+    .min(1, { message: '例文は必須です' }),
+  exampleTranslation: z
+    .string()
+    .max(100, { message: '100文字以内で入力してください' })
+    .min(1, { message: '例文の日本語訳は必須です' }),
 });
 
 export type WordFormInput = z.infer<typeof wordFormSchema>;
