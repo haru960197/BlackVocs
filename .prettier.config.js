790c2c1e3ba5bc7787d6bfd9ecbdfde6490193ba
kuro-home-pc <ha.brack96@gmail.com>
2025-11-15 20:40:41 +0900

    :recycle: コメントアウト等を整理

--- .prettier.config.js
+++ .prettier.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  // --- .prettierrc.json からの内容 ---
   tabWidth: 2,
   printWidth: 100,
   trailingComma: "es5",
   semi: true,
   singleQuote: false,
 
-  // --- 元の prettier.config.js の内容 ---
   plugins: [require("prettier-plugin-tailwindcss")],
 };
--- eslint.config.js
+++ eslint.config.js
@@ -2,27 +2,24 @@ const { FlatCompat } = require("@eslint/eslintrc");
 const path = require("path");
 const nextPlugin = require("@next/eslint-plugin-next");
 
-// __dirname は CJS (module.exports を使う) ファイルでは利用可能です
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   resolvePluginsRelativeTo: __dirname,
 });
 
 module.exports = [
-  // FlatCompatの extends() を使って、以前の .eslintrc.json 相当の設定を読み込みます
   ...compat.extends(
-    "plugin:@typescript-eslint/recommended", // TypeScript
-    "prettier"                              // Prettier競合回避 (必ず最後)
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
   ),
 
+  // next/core-web-vitals はFlatCompatでは読み込めない
   nextPlugin.configs["core-web-vitals"],
 
-  // 無視するディレクトリ
   {
     ignores: [
       "node_modules/",
       ".next/",
-      "dist/",
       ".husky/",
     ],
   },
